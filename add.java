273124d7bd95b302a24c59068b222c73568bde7d
alphonsaboban <alphonsaboban33@gmail.com>
2025-03-12 02:19:54 -0700

    JAVALAB

---
+++ add.java
@@ -0,0 +1,11 @@
+public class add
+{
+public static void main(String[] args)
+{
+int a=5,b=4;
+int sum=a+b;
+{
+System.out.println("sum of number is:" +sum);
+}
+}
+}
---
+++ array.java
@@ -0,0 +1,11 @@
+public class array
+{
+public static void main(String[] args)
+{
+int [] numbers={7,8,9};
+for (int i:numbers)
+{
+System.out.println(i);
+}
+}
+}
